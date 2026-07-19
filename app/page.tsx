8285f40556b9c81192ae7c2bec0672e039944651
aess-code <804057074@qq.com>
2026-07-19 22:17:01 +0800

    更新 page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -142,7 +142,6 @@ export default function HomePage() {
     </p>
   </div>
 
-  <div className="flex flex-col sm:flex-row gap-4 mb-10">
     <div className="relative flex-1">
       <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-zinc-500" />
       <input
